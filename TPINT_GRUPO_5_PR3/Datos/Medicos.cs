c61465888695468c0bd7faac36cfadafafee6b6b
eastanganelli <ezequielstanganelli@gmail.com>
2026-06-19 00:11:26 -0300

    Armado de Medico en protecto de Datos y Negocio

--- TPINT_GRUPO_5_PR3/Datos/Medicos.cs
+++ TPINT_GRUPO_5_PR3/Datos/Medicos.cs
@@ -1,5 +1,49 @@
-﻿namespace Datos {
-    internal class Medicos {
+﻿using Entidades;
+using System;
+using System.Data;
 
+namespace Datos {
+    public class Medicos {
+        private AccesoDatos conexion = new AccesoDatos();
+        public Medico ObtenerMedico(int id_Medico)
+        {
+            string consulta = $"SELECT * FROM Medico WHERE id_medico = {id_Medico}";
+            DataTable resultado = conexion.ObtenerTabla(consulta, "Medico");
+
+            if (resultado.Rows.Count == 0) {
+                throw new Exception($"El Medico con ID [{id_Medico}] fue no encontrado!");
+            }
+
+            Medico Medico = null;
+            if (resultado.Rows.Count > 0) {
+                Medico = new Medico {
+                    IDMedico = Convert.ToInt32(resultado.Rows[0]["id_medico"]),
+                    IDPersona = Convert.ToInt32(resultado.Rows[0]["id_persona"]),
+                    Legajo = resultado.Rows[0]["legajo"].ToString(),
+                    IDEspecialidad = Convert.ToInt32(resultado.Rows[0]["id_especialidad"]),
+                    Estado = Convert.ToBoolean(resultado.Rows[0]["activo"])
+                };
+            }
+            return Medico;
+        }
+        public DataTable ObtenerMedicos() {
+            string consulta = "SELECT * FROM Medico";
+            return conexion.ObtenerTabla(consulta, "Medico");
+        }
+        public int ObtenerCantidadDeMedicos() {
+            string consulta = "SELECT COUNT(*) FROM Medico";
+            DataTable resultado = conexion.ObtenerTabla(consulta, "Medico");
+            return Convert.ToInt32(resultado.Rows[0][0]);
+        }
+        public int ObtenerCantidadDePaginas(int cantidad_pagina = 10) {
+            string consulta = "SELECT COUNT(*) FROM Medico";
+            DataTable resultado = conexion.ObtenerTabla(consulta, "Medico");
+            int cantidad_Medicos = Convert.ToInt32(resultado.Rows[0][0]);
+            return (int)Math.Ceiling((double)cantidad_Medicos / cantidad_pagina);
+        }
+        public DataTable ObtenerMedicosPaginado(int nro_pagina, int cantidad_pagina = 10) {
+            string consulta = $"SELECT * FROM Medico ORDER BY id_medico ASC LIMIT {cantidad_pagina} OFFSET (({nro_pagina}) * {cantidad_pagina})";
+            return conexion.ObtenerTabla(consulta, "Medico");
+        }
     }
 }
--- TPINT_GRUPO_5_PR3/Negocios/MedicosNegocio.cs
+++ TPINT_GRUPO_5_PR3/Negocios/MedicosNegocio.cs
@@ -1,10 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Datos;
+using Entidades;
+using System;
+using System.Data;
 
-namespace Negocio
-{
-    internal class MedicosNegocio
-    {
+namespace Negocio {
+    public class MedicosNegocio {
+        private Medicos datosMedicos = new Medicos();
+        public DataTable ObtenerMedicos()
+        {
+            return datosMedicos.ObtenerMedicos();
+        }
+        public int ObtenerCantidadDeMedicos()
+        {
+            return datosMedicos.ObtenerCantidadDeMedicos();
+        }
+        public int ObtenerCantidadDePaginas(int CantPagina)
+        {
+            return datosMedicos.ObtenerCantidadDePaginas(CantPagina);
+        }
+        public DataTable ObtenerMedicosPaginados(int NroPagina, int CantPagina)
+        {
+            return datosMedicos.ObtenerMedicosPaginado(NroPagina, CantPagina);
+        }
+        public Medico ObtenerMedicoPorId(int id_Medico)
+        {
+            try
+            {
+                return datosMedicos.ObtenerMedico(id_Medico);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener el Medico por ID: " + ex.Message);
+            }
+        }
     }
 }
